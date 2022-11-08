35f69d0aaf3b04cc86450145658ec09687e72da3
no-bugs-only-features <dwright2@mail.csuchico.edu>
2022-11-08 14:44:20 -0800

    Added new user model.

--- prototype/core/forms.py
+++ prototype/core/forms.py
@@ -13,3 +13,21 @@ class SignupForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
+
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
--- prototype/core/models.py
+++ prototype/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+#Reference this model using get_user_model() or settings.AUTH_USER_MODEL
+#https://docs.djangoproject.com/en/3.2/topics/auth/customizing/#referencing-the-user-model
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self_):
+        return self.email
